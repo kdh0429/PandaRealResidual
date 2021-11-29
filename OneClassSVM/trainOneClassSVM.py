e81cd55c3bec9058c569d9768261d9007bd5bba2
Donghyeon Kim <kdh0429@snu.ac.kr>
2021-11-29 16:14:12 +0900

    Train OCSVM for a real robot

---
+++ OneClassSVM/trainOneClassSVM.py
@@ -0,0 +1,21 @@
+import numpy as np
+from numpy import genfromtxt
+
+from sklearn.svm import OneClassSVM
+
+import _pickle as cPickle
+
+# Data
+num_train_data = 266000
+num_joint = 7
+
+train_data = genfromtxt('./data/TrainingData.csv', delimiter=',')
+train_idx = np.random.randint(train_data.shape[0], size=num_train_data)
+
+clf = OneClassSVM(gamma='scale', nu=0.000001).fit(train_data[train_idx])
+
+# save the classifier
+with open('./model/ocsvm_residual.pkl', 'wb') as fid:
+    cPickle.dump(clf, fid)    
+
+print("Training Finished!")
